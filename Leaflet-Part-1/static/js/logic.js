57b75dc77610d2b5c346f5cfd2a6a845a3ee48a1
catisf <cati.sanches@gmail.com>
2024-01-31 21:05:01 +0000

    tidied up script

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,31 +1,5 @@
-// Define the url to get the GeoJSON data
-let eqData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// Fetch the data using D3
-d3.json(eqData).then(function (data){
-    console.log(data);
-
-    let eqData = data.features;
-    
-    let markers = [];
-
-    for (let i = 0; i < eqData.length; i++) {
-        // Setting the marker radius for the state by passing population into the markerSize function
-        markers.push(
-          L.circle([eqData[i].geometry.coordinates[1],eqData[i].geometry.coordinates[0]], {
-            stroke: false,
-            fillOpacity: 0.75,
-            color: "white",
-            fillColor: "red",
-            radius: eqData[i].properties.mag*10000
-          }).
-          bindPopup("<h3> Magnitude: " + eqData[i].properties.mag + "</h3>" +
-          "<p> Place: " + eqData[i].properties.place + "</p>")
-        )};
-    
-    // Create marker layer
-    let eqLayer = L.layerGroup(markers);
-
+// Function to the map
+function buildMap(markerLayer){
     // Create a tile layer.
     let streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -38,20 +12,45 @@ d3.json(eqData).then(function (data){
 
     // Create an overlay object.
     let overlayMaps = {
-        "Earthquake Magnitude": eqLayer,
+        "Earthquake Magnitude": markerLayer,
     };
 
     // Define a map object.
     let myMap = L.map("map", {
         center: [37.09, -95.71],
         zoom: 5,
-        layers: [streetMap, eqLayer]
+        layers: [streetMap, markerLayer]
         });
     
     // Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+}
 
+
+// Function to create the markers
+function createMarkers(response){
+    let eqData = response.features;
+    let markers = [];
+
+    for (let i = 0; i < eqData.length; i++) {
+        // Setting the marker radius for the state by passing population into the markerSize function
+        markers.push(
+          L.circle([eqData[i].geometry.coordinates[1],eqData[i].geometry.coordinates[0]], {
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "white",
+            fillColor: "red",
+            radius: eqData[i].properties.mag*20000
+          }).
+          bindPopup("<h3> Magnitude: " + eqData[i].properties.mag + "</h3>" +
+          "<p> Place: " + eqData[i].properties.place + "</p>")
+        )};
     
-});
+    // Create marker layer
+    buildMap(L.layerGroup(markers));
+}
+
+// Fetch the data using D3
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers)
