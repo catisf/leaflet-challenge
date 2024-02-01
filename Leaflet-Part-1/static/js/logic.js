870a20f1176e5746f5901e4483775cb08398b8a5
catisf <cati.sanches@gmail.com>
2024-02-01 16:38:37 +0000

    changed markers colour scale, and added legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,3 +1,32 @@
+// Function to add a legend to the map
+function addLegend(map){
+
+    let legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function() {
+    
+      let div = L.DomUtil.create("div", "info legend");
+      let limits = [-10,10,30,50,70,90];
+      let colors = ["#fde725","#7ad151","#22a884","#2a788e","#414487","#440154"];
+    
+      // Add a header to specify what the legend is showing
+      div.innerHTML = "<h3>Earthquake Depth</h3>";
+
+      // Loop through colours to assign a colour to each limit range
+      for (let i = 0; i < colors.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');}
+
+        return div;
+  };
+  
+    // Adding the legend to the map
+    legend.addTo(map);     
+};
+  
+
+
 // Function to the map
 function buildMap(markerLayer){
     // Create a tile layer.
@@ -26,23 +55,30 @@ function buildMap(markerLayer){
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+    addLegend(myMap)
 }
 
 function markerColor(thisMarker){
-    console.log(thisMarker)
+    
+    // Define a colour for markers based on the depth of the earthquake
+    // Starting with a lighter colour and moving towards darker tones
     if (thisMarker < 10){
-        return "green"
+        return "#fde725"
     } else if (thisMarker < 30){
-        return "blue"
+        return "#7ad151"
     }
     else if (thisMarker < 50){
-        return "yellow"
+        return "#22a884"
     }
     else if (thisMarker < 70){
-        return "orange"
+        return "#2a788e"
+    }
+    else if (thisMarker < 90){
+        return "#414487"
     }
     else{
-        return "red"
+        return "#440154"
     };
 };
 
@@ -52,13 +88,14 @@ function createMarkers(response){
     let eqData = response.features;
     let markers = [];
 
+    // loop through the data to create each circle
     for (let i = 0; i < eqData.length; i++) {
         // Setting the marker radius for the state by passing population into the markerSize function
         markers.push(
           L.circle([eqData[i].geometry.coordinates[1],eqData[i].geometry.coordinates[0]], {
             stroke: false,
             fillOpacity: 0.75,
-            color: "white",
+            color: "black",
             fillColor: markerColor(eqData[i].geometry.coordinates[2]),
             radius: eqData[i].properties.mag*20000
           }).
@@ -71,5 +108,5 @@ function createMarkers(response){
     buildMap(L.layerGroup(markers));
 }
 
-// Fetch the data using D3
+// Fetch the data using D3 and use it to build the map
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers)
